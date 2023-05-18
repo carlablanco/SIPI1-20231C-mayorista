171759608c48f24e9701c7e176de427e543d3fcc
fedese08 <fedesena2000@gmail.com>
2023-05-18 00:51:52 -0300

    product list agregada

---
+++ al-por-menor/front-end/app/(productos)/product-list/layout.tsx
@@ -0,0 +1,21 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {  
+
+  return (
+
+    
+    <>
+      <main className="grow">
+
+        {children}
+
+      </main>
+    </>
+  )
+}
--- al-por-menor/front-end/components/hero.tsx
+++ al-por-menor/front-end/components/hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import MainImage from '@/public/images/undraw_shopping_app_flsj.svg'
+import Link from 'next/link'
 
 export default function Hero() {
   return (
@@ -35,10 +36,10 @@ export default function Hero() {
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Precios mayoristas a tu alcance, sin comprar de más. Suscribite a nuestros planes de compra mensuales y recibilos desde la comodidad de tu casa, al mejor precio.</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Comprá</a>
+                  <Link className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="/product-list">Comprá</Link>
                 </div>
                 <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Suscribite</a>
+                  <Link className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/product-list">Suscribite</Link>
                 </div>
               </div>
             </div>
