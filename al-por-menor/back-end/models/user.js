833a00060c04ef02917fe4d0aa5f2348408f92f1
Carla <88563106+carlablanco@users.noreply.github.com>
2023-05-26 15:10:27 -0300

    Update user.js

--- al-por-menor/back-end/models/user.js
+++ al-por-menor/back-end/models/user.js
@@ -1,31 +1,71 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-
+      // Define associations here
     }
   }
-  User.init({
-    name: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    address: DataTypes.STRING,
-    city: DataTypes.STRING,
-    password: DataTypes.STRING,
-    supplier: DataTypes.BOOLEAN,
-    otpCode: DataTypes.INTEGER,
-    otpExpiration: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [2, 255], // Minimo 2, maximo 20 caracteres
+          is: /^[A-Za-z]+$/ // Valida que solo tenga letras
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [2, 20], // Minimo 2, maximo 20 caracteres
+          is: /^[A-Za-z]+$/ // Valida que solo tenga letras
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true, // Valida que sea un formato mail
+        },
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [6, 30], // Minimo 6, maximo 30 caracteres
+        },
+      },
+      supplier: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      otpCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      otpExpiration: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+
   return User;
 };
