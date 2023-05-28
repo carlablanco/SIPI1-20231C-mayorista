ad1bb7c67aebc98920d12284fc40fccddd1da39b
Sebastián Medina <sebastianmedinao@hotmail.com>
2023-05-28 19:02:43 -0300

    fixes

--- al-por-menor/back-end/routes.js
+++ al-por-menor/back-end/routes.js
@@ -7,7 +7,8 @@ const SubscriptionController = require("./controllers/subscription.controller")
 
 router.post('/login', UserController.userLogIn);
 router.post('/register', UserController.userRegister);
+
 router.get('/user/:userId/subscriptions', SubscriptionController.getUserSubscriptionWithProducts);
-router.get('/subscriptions', SubscriptionController.getUserSubscriptionWithProducts);
+router.get('/subscriptions', SubscriptionController.getAllSubscriptionsWithProducts);
 
 module.exports = router;
