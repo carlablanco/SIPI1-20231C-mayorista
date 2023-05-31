202728a7a9e65796e31d4a3fae0f5cf544de19f3
Sergio Garroni <serg2404@gmail.com>
2023-05-30 23:06:59 -0300

    add interfaces to missing components

--- al-por-menor/front-end/app/subscripcion/[id]/page.tsx
+++ al-por-menor/front-end/app/subscripcion/[id]/page.tsx
@@ -1,39 +1,33 @@
 import React from 'react'
 import Sub from '@/components/sub'
 import Footer from '@/components/ui/footer'
+import { ISubscription } from '@/types/responses'
+import { Cadence } from '@/enums/enums'
+
+interface SubscripcionProps {
+  subscriptionId: string
+}
 
 
-export default function Subscripcion({params}: any) {
-
-    const product = params.id
+export const Subscripcion: React.FC<SubscripcionProps> = ({subscriptionId}) => {
   
-    const producto =
-      {
-        id: {product},
-        nombre: `Pack ${product}`,
-        img: "https://previews.123rf.com/images/prettyvectors/prettyvectors1605/prettyvectors160500174/56975107-alimentos-en-paquete-entrega-de-comida-vector-ilustraci%C3%B3n-de-dibujos-animados-plana.jpg",
-        descripcion: [
-          "- Arroz",
-          "- Fideos",
-          "- Azúcar",
-          "- Yerba",
-          "- Harina",
-          "- Polenta",
-          "- Lentejas",
-          "- Porotos",
-        ],
-        unidad_de_medicion: "kg",
-        marca: "Marca 1",
-        empresa_proveedora: "Empresa 1",
-        categoría: "Categoría 1",
-        precio: 100,
-    }
-    
+  ``
+const subscription: ISubscription = {
+  subscriptionId: subscriptionId,
+  name: "Pack 1",
+  cadence: Cadence.MONTHLY,
+  price: 100,
+  productList:[],
+  numberOfPeople: 1,
+  imgUrl: "https://previews.123rf.com/images/prettyvectors/prettyvectors1605/prettyvectors160500174/56975107-alimentos-en-paquete-entrega-de-comida-vector-ilustraci%C3%B3n-de-dibujos-animados-plana.jpg"
+}
 
   return (
     <div>
-        <Sub id={producto.id} product={producto}/>
+        <Sub id={subscription.subscriptionId} subscription={subscription}/>
         <Footer/>
     </div>
   )
 }
+
+export default Subscripcion;
