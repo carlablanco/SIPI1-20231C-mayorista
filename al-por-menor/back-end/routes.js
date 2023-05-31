272e9114629a797f161d0830cd1ad528aa7729f8
Carla <88563106+carlablanco@users.noreply.github.com>
2023-05-30 23:56:39 -0300

    Update routes.js

--- al-por-menor/back-end/routes.js
+++ al-por-menor/back-end/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Controllers
 const UserController = require("./controllers/user.controller")
 const SubscriptionController = require("./controllers/subscription.controller")
+const productController = require('../controllers/product.controller');
 
 router.post('/login', UserController.userLogIn);
 router.post('/register', UserController.userRegister);
@@ -14,5 +15,12 @@ router.get('/subscriptions', SubscriptionController.getAllSubscriptionsWithProdu
 router.post('/user/:userId/subscriptions', SubscriptionController.subscribeUser)
 router.patch('/user/:userId/subscriptions/:subscriptionId', SubscriptionController.modifyUserSubscription);
 router.delete('/user/:userId/subscriptions/:subscriptionId', SubscriptionController.cancelUserSubscription);
+
+router.get('/subscriptions/:subscriptionId/products', productController.getProductsForSubscription);
+router.get('/products/:productId', productController.getProduct);
+
+router.post('/products', productController.createProduct);
+router.put('/products/:productId', productController.updateProduct);
+router.delete('/products/:productId', productController.deleteProduct);
 
 module.exports = router;
