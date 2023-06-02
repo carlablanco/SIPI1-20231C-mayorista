569f7fbb9004cc28315aae3c93757254217ff22e
Carla <88563106+carlablanco@users.noreply.github.com>
2023-06-02 12:37:52 -0300

    cleaned up profile

--- al-por-menor/front-end/components/Profile.tsx
+++ al-por-menor/front-end/components/Profile.tsx
@@ -37,36 +37,6 @@ const ProfilePage: React.FC = () => {
     subscriptions: [
       {
         userId: "1",
-        subscriptionId: "1",
-        cadence: Cadence.MONTHLY,
-        numberOfPeople: 1,
-        name: "Pack 1",
-        price: 100,
-        imgUrl: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-        productList: [],
-      },
-      {
-        userId: "1",
-        subscriptionId: "2",
-        cadence: Cadence.WEEKLY,
-        numberOfPeople: 4,
-        name: "Pack 2",
-        price: 200,
-        imgUrl: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-        productList: [],
-      },
-      {
-        userId: "1",
-        subscriptionId: "3",
-        cadence: Cadence.BIWEEKLY,
-        numberOfPeople: 2,
-        name: "Pack 3",
-        price: 300,
-        imgUrl: "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-        productList: [],
-      },
-      {
-        userId: "1",
         subscriptionId: "4",
         cadence: Cadence.BIMONTHLY,
         numberOfPeople: 3,
@@ -87,59 +57,6 @@ const ProfilePage: React.FC = () => {
   const setIsEditingFalse = () => {
     setIsEditing(false);
   }
-
-  const calculatePrice = (subscription: ISubscription) => {
-    //TO-DO call api to get price
-    return subscription.price * subscription.numberOfPeople;
-  }
-
-  // Update subscription cadence
-  const updateCadence = (subscriptionId: string, cadence: Cadence) => {
-    setIsEditingTrue();
-    setUser((prevUser) => {
-      const updatedSubscriptions = prevUser.subscriptions.map((subscription) =>
-        subscription.subscriptionId === subscriptionId
-          ? { ...subscription, cadence }
-          : subscription
-      );
-      updatePrice(subscriptionId);
-      return { ...prevUser, subscriptions: updatedSubscriptions };
-    });
-  };
-
-  // Update subscription numberOfPeople
-  const updateNumberOfPeople = (subscriptionId: string, numberOfPeople: number) => {
-    setIsEditingTrue();
-    setUser((prevUser) => {
-      const updatedSubscriptions = prevUser.subscriptions.map((subscription) =>
-        subscription.subscriptionId === subscriptionId
-          ? { ...subscription, numberOfPeople }
-          : subscription
-      );
-      updatePrice(subscriptionId);
-      return { ...prevUser, subscriptions: updatedSubscriptions };
-    });
-  };
-
-  const updatePrice = (subscriptionId: string) => {
-    setUser((prevUser) => {
-      const updatedSubscriptions = prevUser.subscriptions.map((subscription) =>
-        subscription?.subscriptionId === subscriptionId && subscription.numberOfPeople
-          ? { ...subscription, price: calculatePrice(subscription) }
-          : subscription
-      );
-      return { ...prevUser, subscriptions: updatedSubscriptions };
-    });
-  }
-
-  const deleteSubscription = (subscriptionId: string) => {
-    setUser((prevUser) => {
-      const updatedSubscriptions = prevUser.subscriptions.filter((subscription) =>
-        subscription.subscriptionId !== subscriptionId
-      );
-      return { ...prevUser, subscriptions: updatedSubscriptions };
-    });
-  };
 
 
   return (
@@ -163,40 +80,7 @@ const ProfilePage: React.FC = () => {
             readOnly={false} // Cambia readOnly a false
           />
         </div>
-        <div className="profile-input">
-          <label htmlFor="address">Dirección:</label>
-          <input
-            type="text"
-            id="address"
-            value={user.address}
-            readOnly={false} // Cambia readOnly a false
-          />
-        </div>
-        <div className="profile-input">
-          <label htmlFor="city">Ciudad:</label>
-          <input
-            type="text"
-            id="city"
-            value={user.city}
-            readOnly={false} // Cambia readOnly a false
-          />
-        </div>
-      </div>
-  
-      <div className="profile-details">
-        <h2 className="text-2xl font-bold mb-4">Subscripciones activas:</h2>
-        <div className="subscription-list">
-          {user.subscriptions.map((subscription) => (
-            <div className="subscription-container" key={subscription.subscriptionId}>
-              <SubscriptionComponent
-                subscription={subscription}
-                updateCadence={updateCadence}
-                updateNumberOfPeople={updateNumberOfPeople}
-                deleteSubscription={deleteSubscription}
-              />
-            </div>
-          ))}
-        </div>
+        
       </div>
     </div>
   );  
