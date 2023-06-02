c6958df02ec16f520632921ccdeeb366a3206f62
Carla Blanco <88563106+carlablanco@users.noreply.github.com>
2023-06-02 00:08:42 -0300

    (BACKEND) Product Endpoints basicos
    * product basic endpoints
    
    * Update routes.js
    
    * removed suscription

---
+++ al-por-menor/back-end/controllers/product.controller.js
@@ -0,0 +1,57 @@
+const productService = require('../services/product.service');
+
+async function getProduct(req, res, next) {
+    const { productId } = req.params;
+
+    try {
+        const product = await productService.getProduct(productId);
+        res.json(product);
+    } catch (error) {
+        console.error('Error retrieving product:', error);
+        next(error);
+    }
+}
+
+async function createProduct(req, res, next) {
+    const { name, description, measurementUnit, brand, supplyingCompany, category, price, imgUrl } = req.body;
+
+    try {
+        const product = await productService.createProduct({ name, description, measurementUnit, brand, supplyingCompany, category, price, imgUrl });
+        res.status(201).json(product);
+    } catch (error) {
+        console.error('Error creating product:', error);
+        next(error);
+    }
+}
+
+async function updateProduct(req, res, next) {
+    const { productId } = req.params;
+    const { name, description, measurementUnit, brand, supplyingCompany, category, price, imgUrl } = req.body;
+
+    try {
+        const product = await productService.updateProduct(productId, { name, description, measurementUnit, brand, supplyingCompany, category, price, imgUrl });
+        res.json(product);
+    } catch (error) {
+        console.error('Error updating product:', error);
+        next(error);
+    }
+}
+
+async function deleteProduct(req, res, next) {
+    const { productId } = req.params;
+
+    try {
+        await productService.deleteProduct(productId);
+        res.sendStatus(204);
+    } catch (error) {
+        console.error('Error deleting product:', error);
+        next(error);
+    }
+}
+
+module.exports = {
+    getProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+};
