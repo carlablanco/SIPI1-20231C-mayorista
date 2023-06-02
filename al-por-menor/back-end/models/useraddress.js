d650cc9a6003cf564107755be4a900ac998787fe
Carla <88563106+carlablanco@users.noreply.github.com>
2023-06-01 23:59:09 -0300

    validaciones

--- al-por-menor/back-end/models/useraddress.js
+++ al-por-menor/back-end/models/useraddress.js
@@ -1,31 +1,52 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class useraddress extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
-  useraddress.init({
-    userId: DataTypes.INTEGER,
-    addressAlias: DataTypes.STRING,
-    province: DataTypes.STRING,
-    locality: DataTypes.STRING,
-    street: DataTypes.STRING,
-    number: DataTypes.STRING,
-    postalCode: DataTypes.STRING,
-    apartmentNumberAndFloor: DataTypes.STRING,
-    cellphoneNumber: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'useraddress',
-  });
+
+  useraddress.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'userId is required',
+          },
+          isInt: {
+            msg: 'userId must be an integer',
+          },
+        },
+      },
+      addressAlias: DataTypes.STRING,
+      province: DataTypes.STRING,
+      locality: DataTypes.STRING,
+      street: DataTypes.STRING,
+      number: DataTypes.STRING,
+      postalCode: DataTypes.STRING,
+      apartmentNumberAndFloor: DataTypes.STRING,
+      cellphoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'cellphoneNumber is required',
+          },
+          isNumeric: {
+            msg: 'cellphoneNumber must be a numeric value',
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'useraddress',
+    }
+  );
+
   return useraddress;
 };
--- al-por-menor/back-end/models/userpaymentmethod.js
+++ al-por-menor/back-end/models/userpaymentmethod.js
@@ -1,28 +1,83 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class userpaymentmethod extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
-  userpaymentmethod.init({
-    userId: DataTypes.INTEGER,
-    cardAlias: DataTypes.STRING,
-    cardHolderName: DataTypes.STRING,
-    cardNumber: DataTypes.STRING,
-    expirationDate: DataTypes.STRING,
-    CVV: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'userpaymentmethod',
-  });
+
+  userpaymentmethod.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'userId is required',
+          },
+          isInt: {
+            msg: 'userId must be an integer',
+          },
+        },
+      },
+      cardAlias: DataTypes.STRING,
+      cardHolderName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'cardHolderName is required',
+          },
+        },
+      },
+      cardNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'cardNumber is required',
+          },
+          isCreditCard: {
+            msg: 'cardNumber must be a valid credit card number',
+          },
+        },
+      },
+      expirationDate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'expirationDate is required',
+          },
+          isDate: {
+            msg: 'expirationDate must be a valid date',
+          },
+        },
+      },
+      CVV: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'CVV is required',
+          },
+          isNumeric: {
+            msg: 'CVV must be a numeric value',
+          },
+          len: {
+            args: [3, 4],
+            msg: 'CVV must have 3 or 4 digits',
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'userpaymentmethod',
+    }
+  );
+
   return userpaymentmethod;
 };
