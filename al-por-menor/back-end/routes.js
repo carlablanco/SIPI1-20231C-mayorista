b0bd8383078ad4b93f0f710eb2997afb97d95cb5
Sebastián Medina <sebastianmedinao@hotmail.com>
2023-06-02 19:46:43 -0300

    Route correction

--- al-por-menor/back-end/routes.js
+++ al-por-menor/back-end/routes.js
@@ -11,16 +11,16 @@ router.post('/register', UserController.userRegister);
 router.get('/products/:productId', productController.getProduct);
 // User Address routes
 router.post('/user/address', userController.createUserAddress);
-router.put('/user/address/:addressId', userController.updateUserAddress);
+router.patch('/user/address/:addressId', userController.updateUserAddress);
 router.delete('/user/address/:addressId', userController.deleteUserAddress);
 
 // User Payment Method routes
 router.post('/user/payment-method', userController.createUserPaymentMethod);
-router.put('/user/payment-method/:paymentMethodId', userController.updateUserPaymentMethod);
+router.patch('/user/payment-method/:paymentMethodId', userController.updateUserPaymentMethod);
 router.delete('/user/payment-method/:paymentMethodId', userController.deleteUserPaymentMethod);
 
 router.post('/products', productController.createProduct);
-router.put('/products/:productId', productController.updateProduct);
+router.patch('/products/:productId', productController.updateProduct);
 router.delete('/products/:productId', productController.deleteProduct);
 
 module.exports = router;
