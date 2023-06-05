8bb63a658ec3030dbb7cc51953eb0e04abbd930d
Carla <88563106+carlablanco@users.noreply.github.com>
2023-06-05 15:15:15 -0300

    Revert "aa"
    This reverts commit b4a6bc76158a70cf6c8080868b5ca7f9e297f463.

--- al-por-menor/front-end/components/Product.tsx
+++ al-por-menor/front-end/components/Product.tsx
@@ -58,7 +58,7 @@ export const Product: React.FC<ProductProps> = ({product}) => {
   useEffect(() => {
     getCurrentPrice(product.priceList);
     setInterval(() => {
-      setRemainingTime(getDateDifference(product.promotionEndDate));
+      setRemainingTime(getDateDifference(product.endDate));
     }, 1000);
   }, [unitsSold]);
 
@@ -69,7 +69,7 @@ export const Product: React.FC<ProductProps> = ({product}) => {
     <Link href={`subscripcion/${product.productId}`} className="group" key={product.productId} data-aos="zoom-y-out">
         <div className='product-info'>
           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src={product.imgUrl} className="h-full w-full object-cover object-center group-hover:opacity-75"/>
+            <img src={product.imgUrl} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." className="h-full w-full object-cover object-center group-hover:opacity-75"/>
           </div>
           <h2 className="mt-4 text-xl font-bold text-gray-700">{product.name}</h2>
           { remainingTime?.days > 0 ? <h3 className="mt-1 text-lg font-medium text-gray-900"><AccessTimeIcon/> Finaliza en {remainingTime?.days} dias</h3> : null}
