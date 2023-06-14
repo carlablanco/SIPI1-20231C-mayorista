830154ff3e64f9685afd19950d96a53b85953843
Carla <88563106+carlablanco@users.noreply.github.com>
2023-06-13 22:02:24 -0300

    dropdown

--- al-por-menor/front-end/app/pago-envio/page.tsx
+++ al-por-menor/front-end/app/pago-envio/page.tsx
@@ -1,81 +1,94 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import "./stylesPE.css";
 import Link from 'next/link';
 import swal from 'sweetalert';
-
-
+import { userAddresses, userPaymentMethods } from '../../mock/mockdata';
 
 export default function PagoEnvio() {
+    const [selectedPaymentMethod, setSelectedPaymentMethod] = useState('');
+    const [selectedAddress, setSelectedAddress] = useState('');
+    const [selectedAddressData, setSelectedAddressData] = useState(null);
+    const [selectedPaymentMethodData, setSelectedPaymentMethodData] = useState(null);
 
     function handleAlert() {
-        swal('Gracias por subscribirte! ❤', 'Tu subscripción se realizado correctamente', 'success')
+        swal('Gracias por tu compra! ❤', 'Te estaremos enviando un correo con la confirmación.', 'success')
     }
-    
-    
-  return (
+
+    const userLoggedInId = 1; // Replace with the ID of the logged-in user
+
+    const filteredAddresses = userAddresses.filter(address => address.userId === userLoggedInId);
+    const filteredPaymentMethods = userPaymentMethods.filter(paymentMethod => paymentMethod.userId === userLoggedInId);
+
+    const handleAddressSelection = (addressAlias) => {
+        const selectedAddress = userAddresses.find(address => address.addressAlias === addressAlias);
+        setSelectedAddressData(selectedAddress);
+        setSelectedAddress(addressAlias);
+    };
+
+    const handlePaymentMethodSelection = (cardAlias) => {
+        const selectedPaymentMethod = userPaymentMethods.find(paymentMethod => paymentMethod.cardAlias === cardAlias);
+        setSelectedPaymentMethodData(selectedPaymentMethod);
+        setSelectedPaymentMethod(cardAlias);
+    };
+
+    return (
         <div className='payment-container'>
-            <div className="credit-card-info--form">
-                <div className="input_container">
-                <h2>Informacion de compra</h2>
-                    <label htmlFor="cardholder_name" className="input_label">Nombre completo que figura en la tarjeta</label>
-                    <input id="cardholder_name" className="input_field" type="text" name="cardholder_name" title="Input title" placeholder="Nombre del titular" />
-                </div>
-                <div className="input_container">
-                    <label htmlFor="card_number" className="input_label">Numero de tarjeta</label>
-                    <input id="card_number" className="input_field" type="number" name="card_number" title="Input title" placeholder="0000 0000 0000 0000" />
-                </div>
-                <div className="input_container">
-                    <label htmlFor="expiry_date" className="input_label">Fecha de vencimiento / CVV</label>
-                    <div className="split">
-                    <input id="expiry_date" className="input_field" type="text" name="expiry_date" title="Expiry Date" placeholder="01/23" />
-                    <input id="cvv" className="input_field" type="number" name="cvv" title="CVV" placeholder="CVV" />
-                    </div>
-                </div>
-
-            </div>
             <div>
-            <h2>Informacion de envio</h2>
-                <form className="form" >
-            
+                <h2>Informacion de envío</h2>
+                <form className="form">
                     <div className="flex nya">
-                        <label className='nameEnvio'>
-                            <input required placeholder ="Provincia" type="text" className="input"/>
-                        </label>
-
-                        <label className='lastEnvio'>
-                            <input required placeholder="Localidad" type="text" className="input"/>
-                        </label>
-                    </div>  
-                      
-                    <label>
-                        <input required type="text" placeholder="Calle" className="input"/>
-                    </label>
-                    <div className='flex nya'>
-                        <label>
-                            <input required type="text" placeholder="Numero" className="input"/>
-                        </label>
-                        <label>
-                            <input required type="text" placeholder="CP" className="input"/>
-                        </label>
-                        <label>
-                            <input type="text" placeholder="Piso/Depto" className="input"/>
-                        </label>
-
+                        <div className="input_container">
+                            <label className='nameEnvio'>
+                                <select
+                                    required
+                                    value={selectedAddress}
+                                    onChange={(e) => handleAddressSelection(e.target.value)}
+                                    className="input"
+                                >
+                                    <option value="">Seleccione una dirección</option>
+                                    {filteredAddresses.map(address => (
+                                        <option key={address.addressAlias} value={address.addressAlias}>{address.addressAlias}</option>
+                                    ))}
+                                </select>
+                            </label>
+                            {selectedAddressData && (
+                                <div className="pretty-text">
+                                    <p>Provincia: {selectedAddressData.province}</p>
+                                    <p>Localidad: {selectedAddressData.locality}</p>
+                                    <p>Calle: {selectedAddressData.street}</p>
+                                    <p>Número: {selectedAddressData.number}</p>
+                                    <p>Código Postal: {selectedAddressData.postalCode}</p>
+                                    <p>Departamento y Piso: {selectedAddressData.apartmentNumberAndFloor}</p>
+                                    <p>Teléfono: {selectedAddressData.cellphoneNumber}</p>
+                                </div>
+                            )}
+                        </div>
+                        <div className="input_container">
+                            <label className='lastEnvio'>
+                                <select
+                                    required
+                                    value={selectedPaymentMethod}
+                                    onChange={(e) => handlePaymentMethodSelection(e.target.value)}
+                                    className="input"
+                                >
+                                    <option value="">Seleccione un método de pago</option>
+                                    {filteredPaymentMethods.map(paymentMethod => (
+                                        <option key={paymentMethod.cardAlias} value={paymentMethod.cardAlias}>{paymentMethod.cardAlias}</option>
+                                    ))}
+                                </select>
+                            </label>
+                            {selectedPaymentMethodData && (
+                                <div className="pretty-text">
+                                    <p>Titular de la tarjeta: {selectedPaymentMethodData.cardHolderName}</p>
+                                    <p>Número de tarjeta: **** **** **** {selectedPaymentMethodData.cardNumber.slice(-4)}</p>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                        
-                    <label>
-                        <input required type="tel" placeholder="Numero de celular" className="input"/>
-                    </label>
-                    <label>
-                        <textarea required placeholder="Mensaje adicional" className="input01"></textarea>
-                    </label>
-        
-                    <Link href={'/'} className="purchase--btn" onClick={()=> handleAlert()} >Suscribirse</Link>
-
+                    <Link href={'/'} className="purchase--btn" onClick={() => handleAlert()} >Comprar</Link>
                 </form>
             </div>
         </div>
-
-  );
+    );
 }
